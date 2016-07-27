b9c3985e1cceb0b52739579f1d9e6e990e64085a
Salah Eddine Lahniche <mxpn88@gmail.com>
2016-07-27 15:40:51 +0100

    adding the tests file, it contains the main tests that should be excueted

---
+++ tests.py
@@ -0,0 +1,94 @@
+import pickle
+import os
+import os.path
+import core
+import core.parser
+
+# check wether we can parse the dico file returns 1 if success
+def test1():
+	# remove if dico and choices files already exists
+	if os.path.isfile('dicoN'):
+		os.remove('dicoN')
+	if os.path.isfile('dicoN.choices'):
+		os.remove('dicoN.choices')
+
+	# parse the file and print the results of unpickled dicoN
+	core.parser.parse('dico', 'dicoN')
+	f = open('dicoN', 'rb')
+	i = pickle.load(f)
+	print(i)
+	dico = pickle.load(f)
+	core.printDico(dico)
+
+	ans = input("is everything works fine:")
+	if not ans == "":
+		# faillure
+		return -1
+
+	# print the results of unpickled dicoN.choices
+	f = open('dicoN.choices', 'rb')
+	dico = pickle.load(f)
+	core.printDico(dico)
+
+	ans = input("is everything works fine:")
+	if not ans == "":
+		# faillure
+		return -1
+
+	# succcess
+	return 1
+
+# check wether we can parse the dico file and append the results
+def test2():
+	# remove if dico and choices files already exists
+	if os.path.isfile('dicoN'):
+		os.remove('dicoN')
+	if os.path.isfile('dicoN.choices'):
+		os.remove('dicoN.choices')
+
+	# parse the file and print the results of unpickled dicoN
+	core.parser.parse('dico', 'dicoN')
+	core.parser.parse('dico', 'dicoN')
+	f = open('dicoN', 'rb')
+	i = pickle.load(f)
+	print(i)
+	dico = pickle.load(f)
+	core.printDico(dico)
+
+	ans = input("is everything works fine:")
+	if not ans == "":
+		# faillure
+		return -1
+
+	# print the results of unpickled dicoN.choices
+	f = open('dicoN.choices', 'rb')
+	dico = pickle.load(f)
+	core.printDico(dico)
+
+	ans = input("is everything works fine:")
+	if not ans == "":
+		# faillure
+		return -1
+
+	# succcess
+	return 1
+
+def dispose():
+	if os.path.isfile('dicoN'):
+		os.remove('dicoN')
+	if os.path.isfile('dicoN.choices'):
+		os.remove('dicoN.choices')
+
+
+
+# de the tests
+ans = test1()
+if ans == 1:
+	ans = test2()
+
+
+if ans == 1:
+	print("\ntests passed successfully")
+
+# removes files
+dispose()
