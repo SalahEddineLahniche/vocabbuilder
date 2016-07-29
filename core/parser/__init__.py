3974615104421bbf490f6efeaadf87d0224b0a80
Salah Eddine Lahniche <mxpn88@gmail.com>
2016-07-29 13:10:39 +0100

    adding .words extention to dico files

--- core/parser/__init__.py
+++ core/parser/__init__.py
@@ -2,45 +2,50 @@ import os.path
 import os
 import pickle
 
-#takes a file with the format of
-#word;choice1.correct choice2=.choice3
-##section
-#word;choice1.correct choice2=.choice3
-#and transforme it to a file
-# last index:int
-# {id: word}
+
 def parse(path, destination):
+	'''
+	takes a file with the format of
+	word;choice1.correct choice2=.choice3
+	#section
+	word;choice1.correct choice2=.choice3
+	and transforme it to a file
+	last index:int
+	{id: word}
+	returns -1 if dico file not found
+	'''
 	try:
+		destinationw = destination + '.words'
 		# check whether the files exists & init the destination if not already initialised
 		if not os.path.isfile(path):
 			return -1
-		if not os.path.isfile(destination):
-			g = open(destination, "wb")
+		if not os.path.isfile(destinationw):
+			g = open(destinationw, "wb")
 			pickle.dump(0, g)
 			pickle.dump({}, g)
 			g.close()
 		# open the files
 		f = open(path)
-		g = open(destination, "rb")
-		# unpickle the destination file and increment the last index
+		g = open(destinationw, "rb")
+		# unpickle the destinationw file and increment the last index
 		i = pickle.load(g)
 		i += 1
 		lst = pickle.load(g)
 		g.close()
-		g = open(destination, 'wb')
+		g = open(destinationw, 'wb')
 		# extract the words from the 'path' file and put them in a list
 		dico = []
-		tmpLine = f.readline()[:-1]
+		tmpLine = f.readline()
 		tmpArray = []
 		j = 0
 		while tmpLine:
 			if(tmpLine[0] == "#"):
-				tmpLine = f.readline()[:-1]
+				tmpLine = f.readline()
 				continue
 			tmpArray = tmpLine.split(";")
 			dico.append([tmpArray[1].split("."), tmpArray[0]])
 			j +=  1 + len(dico[-1][0])
-			tmpLine = f.readline()[:-1]
+			tmpLine = f.readline()
 		# update the choices file
 		addChoices(destination, dico, i)
 		# append the new words from the 'path' file
@@ -48,6 +53,10 @@ def parse(path, destination):
 			lst[i] = key[1]
 			i += 1
 			for word in key[0]:
+				if word[-1] == "\n":
+					word = word[:-1]
+				if word[-1] == "\r":
+					word = word[:-1]
 				if word[-1] == "=":
 					word = word[:-1]
 				lst[i] = word
