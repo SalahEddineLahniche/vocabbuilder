03a32ca6a89a998c40e8910f869d9a84698e4382
Salah Eddine Lahniche <mxpn88@gmail.com>
2016-07-29 19:05:49 +0100

    adding a printWord function to print the word object

--- core/__init__.py
+++ core/__init__.py
@@ -5,3 +5,13 @@ def printDico(dico):
 	for key in dico:
 		print(key, dico[key])
 	print("-------")
+
+def printWord(word, debug=False):
+	if debug:
+		print(word[0])
+	print("{} means:".format(word[1][0]))
+	j = 0
+	for i, choice in enumerate(word[1][1]):
+		print("{} - {}".format(i, choice))
+		j = i
+	print("{} - {}".format(j + 1, "I don't know"))
