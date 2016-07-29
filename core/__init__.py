3ef692b25d731d1deb6e6c09767b0a16bfd71946
Salah Eddine Lahniche <mxpn88@gmail.com>
2016-07-29 20:29:31 +0100

    added the config class

--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,6 @@
+import os
+import os.path
+import pickle
 
 # print dico function
 def printDico(dico):
@@ -15,3 +18,23 @@ def printWord(word, debug=False):
 		print("{} - {}".format(i, choice))
 		j = i
 	print("{} - {}".format(j + 1, "I don't know"))
+
+class config(object):
+	"""docstring for config"""
+	def __init__(self, path):
+		super(config, self).__init__()
+
+		self.path = path
+		self.levels = []
+
+		if not os.path.isfile(path):
+			f = open(path, 'wb')
+			pickle.dump(self.levels, f)
+			f.close()
+
+		f = open(path, 'rb')
+		self.levels = pickle.load(f)
+
+	def save(self):
+		f = open(path, 'wb')
+		pickle.dump(self.levels, f)
