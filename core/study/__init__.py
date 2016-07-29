6204053c52d1189e2d6f2473dd664b41c645c9b9
Salah Eddine Lahniche <mxpn88@gmail.com>
2016-07-29 19:07:02 +0100

    adding a reset function & doing some refactoring for existing functions

--- core/study/__init__.py
+++ core/study/__init__.py
@@ -9,7 +9,7 @@ class level(object):
 
 	def __init__(self, path):
 		super(level, self).__init__()
-
+		# initialise variables
 		self.path = path
 		self.words = {}
 		self.choices = {}
@@ -17,7 +17,7 @@ class level(object):
 		self.needsReview = []
 		self.wordsleft = []
 		self.counter = 0
-
+		# check whether .words & .choices files exists & init the .dat file if not found
 		if not os.path.isfile(path + '.words'):
 			raise Exception(path + '.words' + ' not found')
 		if not os.path.isfile(path + '.choices'):
@@ -28,7 +28,7 @@ class level(object):
 			pickle.dump(self.needsReview, f)
 			pickle.dump(self.wordsleft, f)
 			f.close()
-
+		# fill the variables from the files
 		f = open(path + '.words', 'rb')
 		pickle.load(f) # last index, we r not gonna need it here
 		self.words = pickle.load(f)
@@ -41,13 +41,14 @@ class level(object):
 		self.needsReview = pickle.load(f)
 		self.wordsleft = pickle.load(f)
 		f.close()
-
+		# check whether the progress in the level is consistent ie every word is either mastered or needsReview or not prompted yet 
 		if len(self.wordsleft) + len(self.mastered) + len(self.needsReview) < len(self.words):
 			self.wordsleft = list(self.choices.keys())
 			self.mastered = []
 			self.needsReview = []
 
 	def Save(self):
+		# save progress
 		f = open(path + '.dat', "wb")
 		pickle.dump(self.mastered, f)
 		pickle.dump(self.needsReview, f)
@@ -55,32 +56,61 @@ class level(object):
 		f.close()
 
 	def addMastered(self, wordId):
+		# add the word to the mastered set
 		if wordId in self.wordsleft:
 			self.wordsleft.remove(wordId)
 		if wordId in self.needsReview:
 			self.needsReview.remove(wordId)
 		self.mastered += [wordId]
 
+	def reset(self):
+		# make things as it should be in first place
+		self.wordsleft = list(self.choices.keys())
+		self.mastered = []
+		self.needsReview = []
+
+	def addNeedsReview(self, wordId):
+		# add the word to the needs review set
+		if wordId in self.wordsleft:
+			self.wordsleft.remove(wordId)
+		if wordId in self.mastered:
+			self.mastered.remove(wordId)
+		self.needsReview += [wordId]
+
 
 
 	def getWord(self):
-		if self.counter % 10 == 0 and len(self.mastered) > 0:
-			r = rnd.randrange(0, len(self.mastered))
+		'''
+		get a word as an object:
+		(
+			wordId, 
+			[
+				word, 
+				[choice1, choice2, choice3, choice4],
+				correct choice
+			]
+		)
+		'''
+		while True: # if it didn't find a word with the specific index, increase it and try again
+			self.counter += 1
+			if self.counter % 10 == 0 and len(self.mastered) > 0: # each 10 times review a mastred word
+				r = rnd.randrange(0, len(self.mastered))
+				return (self.mastered[r],
+					[self.words[self.mastered[r]],
+					[self.words[i] for i in self.choices[list(self.mastered)[r]][0]],
+					self.words[self.choices[list(self.mastered)[r]][1]]])
+			if self.counter % 4 == 0 and len(self.needsReview) > 0: # each 4 times review a needsReview word
+				r = rnd.randrange(0, len(self.needsReview))
+				return (self.needsReview[r],
+					[self.words[self.needsReview[r]],
+					[self.words[i] for i in self.choices[list(self.needsReview)[r]][0]],
+					self.words[self.choices[list(self.needsReview)[r]][1]]])
+			# get a new word
+			r = rnd.randrange(0, len(self.wordsleft))
 			return (self.wordsleft[r],
 				[self.words[self.wordsleft[r]],
-				[self.words[i] for i in self.choices[list(self.mastered.keys())[r]][0]],
-				self.words[self.choices[list(self.mastered.keys())[r]][1]]])
-		if self.counter % 4 == 0 and len(self.needsReview) > 0:
-			r = rnd.randrange(0, len(self.needsReview))
-			return (self.wordsleft[r],
-				[self.words[self.wordsleft[r]],
-				[self.words[i] for i in self.choices[list(self.needsReview.keys())[r]][0]],
-				self.words[self.choices[list(self.needsReview.keys())[r]][1]]])
-		r = rnd.randrange(0, len(self.wordsleft))
-		return (self.wordsleft[r],
-			[self.words[self.wordsleft[r]],
-			[self.words[i] for i in self.choices[self.wordsleft[r]][0]],
-			self.words[self.choices[self.wordsleft[r]][1]]])
+				[self.words[i] for i in self.choices[self.wordsleft[r]][0]],
+				self.words[self.choices[self.wordsleft[r]][1]]])
 
 
 
